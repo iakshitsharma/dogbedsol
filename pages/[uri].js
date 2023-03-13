64f136c9abcd1b9a3745c26df56c651f5d82db20
iakshitsharma <127782617+iakshitsharma@users.noreply.github.com>
2023-03-14 00:16:10 +0530

    Update [uri].js

--- pages/[uri].js
+++ pages/[uri].js
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 
 export default function SlugPage({ post }) {
 useEffect(()=>{
-  location = `https://www.dogbedsollution.com/${post?.slug}`
+  location = `https://dogbedsollution.com/${post?.slug}`
 },[])
   return (
     <div>
